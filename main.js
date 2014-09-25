9959dbfebd6548ecedf266e3e2206aadc6fd8426
BH_Lin <binghuanlin@gmail.com>
2014-09-26 01:52:26 +0800

    first check-in

---
+++ main.js
@@ -0,0 +1,84 @@
+$(document).ready(function(){
+	var img = document.getElementById('phonePicture');
+	//img.height = (window.innerHeight - 20) +"px";;
+	img.style.height = (window.innerHeight - $("#footer").height() - $("#header").height() -  20) +"px";
+	img.width = '100%';
+
+
+	$("#fingerup").on("touchstart", function() {
+		console.log("1. touchstart");
+		fingerupTouchstart(true);
+	})
+	$("#fingerup").on("touchend", function() {
+		console.log("-1. touchstart");
+		fingerupTouchstart(false);
+	})
+
+
+
+	$("#fingerdown").on("touchstart", function() {
+		console.log("2. touchstart");
+		fingerdownTouchstart(true);
+	})
+	$("#fingerdown").on("touchend", function() {
+		console.log("-2. touchstart");
+		fingerdownTouchstart(false);
+	})
+
+	$("#recover").click(function() {
+		$("#fingerup").show();
+		$("#fingerdown").show();
+		document.getElementById("phonePicture").src = "images/smartphone.png";
+	});
+});
+
+
+var flagup = false;
+var flagdown = false;
+
+function showBendedPhone() {
+	console.log(">>> showBendedPhone");
+	var snd2 = new Audio("sfx/wrong.wav"); // buffers automatically when created
+	snd2.play();
+
+	document.getElementById("phonePicture").src = "images/smartphonebended.png";
+
+	$("#fingerup").hide();
+	$("#fingerdown").hide();
+}
+
+var timerId;
+
+
+var DEFAULT_DURATION = 2500;
+function countdownBending() {
+	var snd = new Audio("sfx/sound1.wav"); // buffers automatically when created
+	snd.play();
+	timerId= setTimeout(showBendedPhone, DEFAULT_DURATION);
+}
+
+function fingerupTouchstart(yes) {
+	flagup = yes;
+
+	if(yes === false) {
+		clearTimeout(timerId);
+	}
+
+	if(flagdown === true) {
+		console.log(">>> fingerupTouchstart");
+		countdownBending();
+	}
+}
+
+function fingerdownTouchstart(yes) {
+	flagup = yes;
+
+	if(yes === false) {
+		clearTimeout(timerId);
+	}
+
+	if(flagup === true) {
+		console.log(">>> fingerdownTouchstart");
+		countdownBending();
+	}
+}
